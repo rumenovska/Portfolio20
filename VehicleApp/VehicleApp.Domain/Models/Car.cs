702cc92a7db447de666e110849da2e79cd8c5392
unknown <marija.rumenovska@hotmail.com>
2020-04-03 14:54:02 +0200

    Models added

--- VehicleApp/VehicleApp.Domain/Models/Car.cs
+++ VehicleApp/VehicleApp.Domain/Models/Car.cs
@@ -3,23 +3,23 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
 
-namespace CarApp.Domain.Models
+namespace VehicleApp.Domain.Models
 {
     public class Car
     {
         public int Id { get; set; }
-        public string VehicleRegistration { get; set; }
+        public string VehicleRegisteredNum { get; set; }
         public string Brand { get; set; }
         public string Color { get; set; }
-        public string Direkcija { get; set; }
-        public string BrojNaShasija { get; set; }
+        public string Directorate { get; set; }
+        public string EngineNum { get; set; }
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
-        public DateTime? PrvaRegistracija { get; set; }
+        public DateTime? FirstRegistration { get; set; }
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
-        public DateTime RegistriranaDo { get; set; }
-        public bool Defekt { get; set; }
-        public bool Zadolzhena { get; set; }
-        public string TipNaVozilo { get; set; }
-        public string ImeNaDolzhnik { get; set; }
+        public DateTime? RegistrationExpiryDate { get; set; }
+        public bool Malfunction { get; set; }
+        public bool Loan { get; set; }
+        public string VehicleType { get; set; }
+        public string EmployeeName { get; set; }
     }
 }
