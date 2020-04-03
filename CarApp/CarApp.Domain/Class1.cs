90baff01326fdab88d23a063d621a0ea6f20ce3f
unknown <marija.rumenovska@hotmail.com>
2020-04-03 14:22:13 +0200

    Vehicle Domain

--- CarApp/CarApp.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CarApp.Domain
-{
-    public class Class1
-    {
-    }
-}
---
+++ VehicleApp/VehicleApp.Domain/Models/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CarApp.Domain.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string VehicleRegistration { get; set; }
+        public string Brand { get; set; }
+        public string Color { get; set; }
+        public string Direkcija { get; set; }
+        public string BrojNaShasija { get; set; }
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime? PrvaRegistracija { get; set; }
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime RegistriranaDo { get; set; }
+        public bool Defekt { get; set; }
+        public bool Zadolzhena { get; set; }
+        public string TipNaVozilo { get; set; }
+        public string ImeNaDolzhnik { get; set; }
+    }
+}
