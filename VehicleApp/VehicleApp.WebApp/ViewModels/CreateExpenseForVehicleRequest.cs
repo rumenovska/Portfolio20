26b94c8f670290033996c4547a5a8a871a6a1810
Marija Rumenovska <marija.rumenovska@hotmail.com>
2020-05-06 15:47:55 +0200

    ExpenceType View Model Updated

--- VehicleApp/VehicleApp.WebApp/ViewModels/CreateExpenseForVehicleRequest.cs
+++ VehicleApp/VehicleApp.WebApp/ViewModels/CreateExpenseForVehicleRequest.cs
@@ -1,10 +1,16 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace VehicleApp.WebApp.ViewModels
 {
     public class CreateExpenseForVehicleRequest
     {
-        public int WehicleId { get; set; }
-        public List<int> ExpenceTypeIds { get; set; }
+        public int VehicleId { get; set; }
+        public int ExpenceTypeId { get; set; }
+        public string Description { get; set; }
+        public double Cost { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+
     }
 }
